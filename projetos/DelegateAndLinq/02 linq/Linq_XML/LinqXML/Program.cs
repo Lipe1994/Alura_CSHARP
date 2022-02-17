34baf7e71599aac2bdbde31a68236d28a865fa30
Filipe <filipe@yardim.com.br>
2022-02-17 03:23:11 -0300

    adicionando experiências com linq e xDocument

--- projetos/DelegateAndLinq/02 linq/Linq_XML/LinqXML/Program.cs
+++ projetos/DelegateAndLinq/02 linq/Linq_XML/LinqXML/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace LinqXML
@@ -21,14 +22,49 @@ namespace LinqXML
                 "</Filme>" +
             "</Filmes>";
 
-            var query = XDocument.Parse(xmlText);
+            var xmlDoc = XDocument.Parse(xmlText);
 
-            var filmes = query.Root;
+            var filmes = xmlDoc.Root;
 
             foreach(var filme in filmes.Elements()) 
             {
                 Console.WriteLine(filme.Element("Diretor").Value);
             }
+
+            Console.WriteLine("\r\n\r\n");
+            Console.WriteLine("Usando sintaxe de consulta, Filme quando diretor for 'James Cameron': ");
+            //Usando sintaxe de consulta
+            var query1 = from f in filmes.Elements()
+                         where (string)f.Element("Diretor") == "James Cameron"
+                         select f;
+
+            foreach (var f in query1)
+            {
+                Console.WriteLine(f.Element("Titulo").Value);
+            }
+
+            Console.WriteLine("\r\n\r\n");
+            Console.WriteLine("Usando sintaxe de função, Filme quando diretor for 'Quentin Tarantino': ");
+            //Usando sintaxe de consulta
+            var query2 = filmes.Elements().Where(f => f.Element("Diretor").Value.Equals("Quentin Tarantino")).Single();
+
+
+            Console.WriteLine(query2.Element("Titulo").Value);
+
+
+            Console.WriteLine("\r\n\r\n");
+            Console.WriteLine("Adicionando um node no doc de consulta");
+            var query3 = (from f in filmes.Elements()
+                         where (string)f.Element("Diretor") == "James Cameron"
+                         select f).Single();
+
+            query3.Add(new XElement("Titulo", "Teste"));
+
+            foreach (var f in query3.Descendants())
+            {
+                Console.WriteLine(f.Value);
+            }
+
         }
     }
 }
