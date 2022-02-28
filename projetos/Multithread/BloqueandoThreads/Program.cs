36d39d368d71aba1cdec2d71614a06071a194740
Lipe <lipe.ferreira1609@hotmail.com>
2022-02-28 20:25:44 -0300

    bloqueando threads

---
+++ projetos/Multithread/BloqueandoThreads/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace BloqueandoThreads
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var contador = new Contador();
+
+            Console.WriteLine("contador: {0}", contador.Numero);
+
+            Thread thread1 = new Thread(()=>
+            {
+                for(int i = 0; i < 50; i++) 
+                {
+                    contador.Incrementar();
+                    Thread.Sleep(i);
+                }
+            });
+
+            Thread thread2 = new Thread(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    contador.Incrementar();
+                    Thread.Sleep(i);
+                }
+            });
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("contador {0}", contador.Numero);
+            Console.ReadLine();
+        }
+    }
+
+    class Contador 
+    {
+        public int Numero { get; private set; } = 0;
+        static object ContadorObject = new object();
+
+        public void Incrementar() 
+        {
+            lock (ContadorObject) 
+            {
+                Numero++;
+            }
+        }
+    }
+}
